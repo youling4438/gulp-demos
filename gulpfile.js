860268b967d7025317782c808c1e6e84d254c2fe
youling4438 <youling0106@qq.com>
2019-09-12 23:37:08 +0800

    add parallel task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require("gulp");
-var { series } = gulp;
+var { series, parallel } = gulp;
 
 // default task
 // gulp.task("default", () => {
@@ -8,31 +8,52 @@ var { series } = gulp;
 
 function clean(cb) {
     console.log("gulp clean running");
-    cb && cb();
+    cb();
 }
 
 function build(cb) {
     console.log("gulp build running");
-    cb && cb();
+    cb();
 }
 
 const copy = cb => {
     console.log("gulp copy running");
-    cb && cb();
+    cb();
 };
 
 const watch = cb => {
     console.log("gulp watch running");
-    cb && cb();
+    cb();
 };
 
 const task1 = cb => {
     console.log("gulp watch running");
-    cb && cb();
+    cb();
 };
 
+const minify = cb => {
+    console.log("gulp minify running");
+    cb();
+};
+
+const livereload = cb => {
+    console.log("gulp livereload running");
+    cb();
+};
 exports.task1 = task1;
 exports.watch = watch;
 exports.build = build;
 exports.copy = copy;
-exports.default = series(clean, build);
+exports.minify = minify;
+/*
+ * series 依次执行每个任务
+ */
+exports.default = series(clean, build, copy, livereload);
+/*
+ * parallel 同时执行多个任务
+ */
+exports.task2 = parallel(copy, watch, task1);
+
+if (process.env.NODE_ENV === "production") {
+    exports.default = series(clean, build, copy, minify);
+}
