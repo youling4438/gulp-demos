ac172f2f974ad0d7e7611f68cf9b8e26d1c1a5e5
youling4438 <youling0106@qq.com>
2019-09-12 23:20:25 +0800

    add function for tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,38 @@
 var gulp = require("gulp");
+var { series } = gulp;
 
-gulp.task("default", () => {
-    console.log("gulp running!");
-});
+// default task
+// gulp.task("default", () => {
+//     console.log("gulp running!");
+// });
+
+function clean(cb) {
+    console.log("gulp clean running");
+    cb && cb();
+}
+
+function build(cb) {
+    console.log("gulp build running");
+    cb && cb();
+}
+
+const copy = cb => {
+    console.log("gulp copy running");
+    cb && cb();
+};
+
+const watch = cb => {
+    console.log("gulp watch running");
+    cb && cb();
+};
+
+const task1 = cb => {
+    console.log("gulp watch running");
+    cb && cb();
+};
+
+exports.task1 = task1;
+exports.watch = watch;
+exports.build = build;
+exports.copy = copy;
+exports.default = series(clean, build);
